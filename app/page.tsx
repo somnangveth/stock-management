ff12598824370c1079376a87d9b5c1aa98a70816
somnangveth <158492587+somnangveth@users.noreply.github.com>
2026-01-10 14:19:01 +0700

    final

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import AuthPage from "./auth/page";
+import AuthForm from "./auth/components/authform";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main>
-        <AuthPage/>
+        <AuthForm/>
       </main>
     </div>
   );
